a840986494cef8bf175aac25364495832b97dc32
Concrete18 <michaelericson19@gmail.com>
2021-11-17 16:38:48 -0500

    added article route

--- backend/routes/api/articles.js
+++ backend/routes/api/articles.js
@@ -2,37 +2,22 @@ const express = require("express");
 const { check } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
-const { Feed, Source } = require("../../db/models");
+const { User,  Feed, Source, sequelize } = require("../../db/models");
 
 const router = express.Router();
 
-// Get source
+// Get feeds with sources included
 router.get('/:id/all', asyncHandler(async (req, res) => {
 	const userId = req.params.id
-	console.log(userId)
 
-	const sources = await Feed.findAll(
+	const feeds = await Feed.findAll(
 		{
 			where: { ownerId: userId },
 			include: Source
 		}
 	);
 
-	console.log(sources)
-
-	const RSS_URL = `https://codepen.io/picks/feed/`;
-
-	fetch(RSS_URL)
-		.then(response => response.text())
-		.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-		.then(data => console.log(data))
-	
-	const { title, body } = req.body;
-
-	return res.json({
-			title,
-			body,
-		});
+	return res.json(feeds);
   }),
 );
 
