2ce94a2106ae4505d66a7a75d9f82a854bb415b6
Concrete18 <michaelericson19@gmail.com>
2021-11-19 12:24:55 -0500

    updated database for articles

--- backend/db/seeders/20211116182354-seed_article.js
+++ backend/db/seeders/20211116182354-seed_article.js
@@ -4,7 +4,11 @@ const articles = []
 for (let i = 0; i <= 10; i++) {
   const obj = {
     sourceId: 1,
-    name: `Test Article ${i}`,
+    title: `Test Article ${i}`,
+    creator: 'Michael Ericson',
+    pubDate: new Date(),
+    content: 'content',
+    contentSnippet: 'content snippet',
     url: 'testArticle.com',
     createdAt: new Date(),
     updatedAt: new Date()
