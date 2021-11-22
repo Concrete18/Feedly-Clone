78ad2b69b77f526f65c50088372e7d2b8fd30d9b
Concrete18 <michaelericson19@gmail.com>
2021-11-22 16:51:54 -0500

    small changes

--- heroku.bash
+++ heroku.bash
@@ -10,7 +10,7 @@ elif [ $1 == 'reset' ]; then
     heroku run npm run sequelize db:migrate
     heroku run npm run sequelize db:seed:all
 elif [ $1 == 'pushseed' ]; then
-    git push heroku main:master
+    git push heroku dev:master
     heroku run npm run sequelize db:seed:undo:all
     heroku run npm run sequelize db:migrate:undo:all
     heroku run npm run sequelize db:migrate
