a8e587705f2e6c6f7b3b62b4697b4e149405d19f
Concrete18 <michaelericson19@gmail.com>
2021-11-24 22:37:14 -0500

    added website name to article entriy

--- frontend/src/components/MainContent/EntryBox/index.js
+++ frontend/src/components/MainContent/EntryBox/index.js
@@ -9,6 +9,7 @@ function EntryBox({ article }) {
       {article.Article.image && <img className='article_image' src={article?.Article.image} alt="article image" />}
       {!article.Article.image && <img className='article_image' src={process.env.PUBLIC_URL + "/assets/placeholder_image.jpg"} alt="placeholder image" />}
       <div className='article article_title'>{article?.Article.title}</div>
+			<div className='article article_website_name'>{article?.Article.websiteName}</div>
 			<div className='article article_snippet'>{article?.Article.contentSnippet}</div>
       {/* <a className='article_link' href={article?.Article?.url}>Visit Website</a> */}
     </div>
