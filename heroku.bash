f9d2bfa325f3cc5ebb77b1602b304bab047be616
Concrete18 <michaelericson19@gmail.com>
2021-11-27 17:12:40 -0500

    added setup command

--- heroku.bash
+++ heroku.bash
@@ -4,6 +4,9 @@ elif [ $1 == 'migrate' ]; then
     heroku run npm run sequelize db:migrate
 elif [ $1 == 'seed' ]; then
     heroku run npm run sequelize db:seed:all
+elif [ $1 == 'setup' ]; then
+    heroku run npm run sequelize db:migrate
+    heroku run npm run sequelize db:seed:all
 elif [ $1 == 'reset' ]; then
     heroku run npm run sequelize db:seed:undo:all
     heroku run npm run sequelize db:migrate:undo:all
