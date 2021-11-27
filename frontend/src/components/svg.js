f0997acb1c5f516afb834e5a383560304c917ca4
Concrete18 <michaelericson19@gmail.com>
2021-11-27 14:01:37 -0500

    added svg file

---
+++ frontend/src/components/svg.js
@@ -0,0 +1,17 @@
+const logoOnly = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none" class="icon icon-accent"><path fill="#2bb24c" d="M16.034 3.334a2.867 2.867 0 00-4.043 0L1.834 13.49a2.867 2.867 0 000 4.043l7.248 7.248c.504.44 1.164.706 1.886.706h6.089c.793 0 1.512-.322 2.032-.841l7.107-7.108a2.867 2.867 0 000-4.043L16.034 3.334zm-.283 18.551L14.736 22.9a.407.407 0 01-.29.12h-.868a.41.41 0 01-.27-.1l-1.033-1.034a.408.408 0 010-.577l1.449-1.45a.41.41 0 01.577 0l1.45 1.45a.409.409 0 010 .577zm0-6.079l-4.054 4.054a.408.408 0 01-.29.12h-.869a.408.408 0 01-.269-.1l-1.034-1.035a.41.41 0 010-.577l4.489-4.488a.409.409 0 01.577 0l1.45 1.45a.409.409 0 010 .576zm0-6.08L8.657 16.82a.408.408 0 01-.29.12H7.5a.407.407 0 01-.27-.1l-1.034-1.035a.409.409 0 010-.577L13.724 7.7a.409.409 0 01.577 0l1.45 1.45a.409.409 0 010 .576z"></path></svg>
+)
+
+const rightArrow = (
+    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="icon icon-black icon-tertiary LeftnavListRow__icon"><path d="M6.432 3.218a.5.5 0 01.638-.058l.07.058 6.428 6.428a.5.5 0 01.058.638l-.058.07-6.429 6.428a.5.5 0 01-.765-.638l.058-.069 6.075-6.076-6.075-6.074a.5.5 0 01-.058-.638l.058-.07z" fill="currentColor" fill-rule="nonzero"></path></svg>
+)
+
+const downArrow = (
+    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="icon icon-black icon-tertiary LeftnavListRow__icon LeftnavListRow__icon--open"><path d="M6.432 3.218a.5.5 0 01.638-.058l.07.058 6.428 6.428a.5.5 0 01.058.638l-.058.07-6.429 6.428a.5.5 0 01-.765-.638l.058-.069 6.075-6.076-6.075-6.074a.5.5 0 01-.058-.638l.058-.07z" fill="currentColor" fill-rule="nonzero"></path></svg>
+)
+
+module.exports = {
+    logoOnly,
+    rightArrow,
+    downArrow
+};
