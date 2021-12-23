1c8a7cdbd2ef5a9846ba8bc28d0855e8ca2c5213
Concrete18 <michaelericson19@gmail.com>
2021-12-22 22:48:47 -0500

    added for testing

---
+++ backend/rssParseTest.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function getMetaData(articleUrl) {
+  const res = await axios.get(articleUrl)
+  const $ = cheerio.load(res.data);
+  metaData = {
+    'image': [
+      'meta[property="og:image"]',
+      'meta[name="parsely-image-url"]',
+      'meta[name="twitter:image"]',
+      'meta[name="twitter:image:src"]'
+    ],
+    'siteName': [
+      'meta[property="og:site_name"]',
+      'meta[name="twitter:site"]'
+    ],
+    'pubDate': [
+      'meta[property="article:published_time"]',
+      'meta[name="parsely-pub-date"]',
+      'meta[name="publish-date"]',
+      'meta[name="pub_date"]'
+    ],
+    'author': [
+      'meta[name="author"]'
+    ],
+    'description': [
+      'meta[property="og:description"]'
+    ]
+  }
+  foundMetaData = {}
+  for (const [name, keys] of Object.entries(metaData)) {
+    for (const key of keys) {
+      content = $(key).attr('content')
+      if (content) {
+        foundMetaData[name] = content
+        continue
+      }
+    }
+  }
+  console.log(foundMetaData)
+  return foundMetaData
+}
+
+// getMetaData('https://blog.playstation.com/2021/12/07/uncharted-legacy-of-thieves-collection-details-on-the-remastered-bundle/')
+// getMetaData('https://www.polygon.com/22832955/succession-and-on-cinema-media')
+// getMetaData('https://www.techradar.com/reviews/the-matrix-resurrections')
+// getMetaData('https://kotaku.com/xbox-game-pass-pc-gets-a-new-name-and-four-more-day-one-1848181194')
+// getMetaData('https://gizmodo.com/5-predictions-for-the-near-future-from-bill-gates-1848191710')
+getMetaData('https://www.nytimes.com/2021/12/21/movies/lucille-ball-being-the-ricardos.html')
+// getMetaData('https://www.wired.com/story/face-recognition-banned-but-everywhere/')
