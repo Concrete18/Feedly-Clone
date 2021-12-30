57451fb81278882e765c2c685bbe3ffea8d6ee49
Concrete18 <michaelericson19@gmail.com>
2021-12-30 15:05:06 -0500

    shrunk down timeSince function

--- frontend/src/components/MainContent/EntryBox/index.js
+++ frontend/src/components/MainContent/EntryBox/index.js
@@ -6,24 +6,16 @@ import ExpandedArticle from '../ExpandedArticleView'
 
 function timeSince(date) {
   const seconds = Math.floor((new Date() - date) / 1000);
-  let interval = seconds / 31536000;
-  // if (interval > 1) {
-  //   return Math.floor(interval) + "y";
-  // }
-  interval = seconds / 2592000;
-  if (interval > 1) {
+  if (seconds / 2592000 > 1) {
     return Math.floor(interval) + "mo";
   }
-  interval = seconds / 86400;
-  if (interval > 1) {
+  if (seconds / 86400 > 1) {
     return Math.floor(interval) + "d";
   }
-  interval = seconds / 3600;
-  if (interval > 1) {
+  if (seconds / 3600 > 1) {
     return Math.floor(interval) + "h";
   }
-  interval = seconds / 60;
-  if (interval > 1) {
+  if (seconds / 60 > 1) {
     return Math.floor(interval) + "min";
   }
   return Math.floor(seconds) + "s";
