c27fb3ddecb411f83b5141d8112b6a1a57d273ca
Concrete18 <michaelericson19@gmail.com>
2022-03-16 02:20:24 -0400

    autoformatted

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { Modal } from '../../context/Modal';
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
 
 // components
-import LoginForm from './LoginForm';
+import LoginForm from "./LoginForm";
 
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <button className='login_button' onClick={() => setShowModal(true)}>Log In</button>
+      <button className="login_button" onClick={() => setShowModal(true)}>
+        Log In
+      </button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
