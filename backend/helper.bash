23f353cfa1b034cf9912818be1e40833e8805273
Concrete18 <michaelericson19@gmail.com>
2022-10-18 15:13:14 -0400

    added restart feature

--- backend/helper.bash
+++ backend/helper.bash
@@ -13,6 +13,15 @@ elif [ $1 == 'reset' ]; then
     npx dotenv sequelize db:create
     npx dotenv sequelize db:migrate
     npx dotenv sequelize db:seed:all
+elif [ $1 == 'restart' ]; then
+    npx dotenv sequelize db:seed:undo:all
+    npx dotenv sequelize db:migrate:undo:all
+    npx dotenv sequelize db:drop
+    npx dotenv sequelize db:create
+    npx dotenv sequelize db:migrate
+    npx dotenv sequelize db:seed:all
+    clear
+    npm start
 else
     echo "Unknown arg given. $1 is invalid."
 fi
